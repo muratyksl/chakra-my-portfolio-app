9c12e96fcd1f765dc8708efc4f1cc6a5049c06c3
Murat Yüksel <murat@softcand.com>
2021-01-11 04:16:52 +0300

    project init and summmary component started

---
+++ src/components/TheSummaryComp.tsx
@@ -0,0 +1,55 @@
+import { Box, Flex, Stat, StatLabel, StatNumber, StatHelpText, StatArrow } from '@chakra-ui/react';
+import * as React from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
+
+type Props = {
+    tlTotalValue: number;
+    usdTotalValue: number;
+    increase: number;
+    // rawPieChartData?: {
+    //     value: number;
+    //     currency: string;
+    // };
+};
+
+export const TheSummaryComp: React.FC<Props> = (props: Props) => {
+    const { tlTotalValue, usdTotalValue, increase } = props;
+
+    return (
+        <Box borderWidth="2px" borderRadius="lg" m="3" p="3">
+            <Flex align="center" justify="space-evenly">
+                <Box fontSize="sm">
+                    <PieChart
+                        data={[
+                            { title: 'USD', value: 10, color: '#E38627' },
+                            { title: 'TL', value: 15, color: '#C13C37' },
+                        ]}
+                        label={({ dataEntry }) => dataEntry.title}
+                    />
+                </Box>
+                <Box>
+                    <Flex direction="column">
+                        <Stat mb="3">
+                            <StatLabel>Dollar Value</StatLabel>
+                            <StatNumber>{usdTotalValue}</StatNumber>
+                            <StatHelpText>
+                                <StatArrow type="increase" />
+                                {`${increase}`}
+                            </StatHelpText>
+                        </Stat>
+                        <Stat>
+                            <StatLabel>TL Value</StatLabel>
+                            <StatNumber>{tlTotalValue}</StatNumber>
+                            <StatHelpText>
+                                <StatArrow type="increase" />
+                                {`${increase}`}
+                            </StatHelpText>
+                        </Stat>
+                    </Flex>
+                </Box>
+            </Flex>
+        </Box>
+    );
+};
+
+export default TheSummaryComp;
